fc1bcd49cac080a888c65f67e4f8b7fbe834c721
MirageLyu <1137935006@qq.com>
2020-03-03 12:57:56 +0800

    Merge logfiles

--- launcher.py
+++ launcher.py
@@ -8,3 +8,13 @@ import os
 # RequirementsParser.parse("sample_requirements/files/", 50000)
 # RequirementsParser.reparseOutputRecursively("reqout.log")
 
+out = open("samples.data", "w")
+fa = open("out.log5", "r")
+fb = open("reqout.log5", "r")
+
+linesa = fa.read().splitlines()
+linesb = fb.read().splitlines()
+for line in linesa:
+    print(line, file=out)
+for line in linesb:
+    print(line, file=out)
