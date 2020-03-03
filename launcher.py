d9a6c8935d7c4f2639ca73eb9d0df928778c6b2c
MirageLyu <1137935006@qq.com>
2020-03-03 12:45:15 +0800

    Modify .gitignore

--- launcher.py
+++ launcher.py
@@ -1,8 +1,10 @@
 import DockerfileParser
 import RequirementsParser
+import os
 
 # DockerfileParser.parse("sample_dockerfile/files/", 50000)
-RequirementsParser.reparseOutputRecursively("out.log")
+# RequirementsParser.reparseOutputRecursively("out.log")
 
 # RequirementsParser.parse("sample_requirements/files/", 50000)
-RequirementsParser.reparseOutputRecursively("reqout.log")
+# RequirementsParser.reparseOutputRecursively("reqout.log")
+
